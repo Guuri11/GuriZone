1cacaefaa4d24b8a56f7949659f08ae0b57fcb22
Guuri11 <sergio.gurillo11@gmail.com>
2020-02-03 13:57:03 +0100

    Creado el Default Controller, Producto Controller y Usuario Controller, importada la plantilla de bootstrap y aplicado el css y js al index.html con su header y footer original

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -20,9 +20,14 @@ class DefaultController extends AbstractController
         // retrieve all links
         $productos = $repository->findAll();
 
+        // Obtener productos mas vendidos y nuevos
+        $productosTT = $repository->findBy(['num_ventasProd'],null,5);
+        //$novedades = $repository->findBy(['fechaSalida'],null,5);
+
         // now pass the array of link object to the view
         return $this->render('default/index.html.twig', [
             'productos' => $productos,
+            'productosTT'=>$productosTT
         ]);
     }
 
